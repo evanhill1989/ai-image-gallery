3e6ce7c2fd76e5bb58e371da6cfddb9c9530b3db
Evan Hill <evanhill1989@gmail.com>
2024-08-05 13:09:11 -0400

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/45e1f5f0-7f39-4026-8441-1ca6c3e83ae3-gcgq8.jpg",
@@ -12,12 +13,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="gap w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {mockImages.map((image, index) => (
+          <div key={image.id + "-" + index} className="gap w-48">
             <img src={image.url} className="" alt="astrophotography" />
           </div>
         ))}
