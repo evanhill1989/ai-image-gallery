b29ef6be3583acdb0fc0365578bdf6a8da666982
Evan Hill <evanhill1989@gmail.com>
2024-08-05 13:33:05 -0400

    db schema attached

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://utfs.io/f/45e1f5f0-7f39-4026-8441-1ca6c3e83ae3-gcgq8.jpg",
   "https://utfs.io/f/845f953e-bc7b-4a7b-8d3f-184326303739-kt8mv6.jpg",
@@ -14,19 +16,17 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany();
 
-  console.log(posts);
+  console.log(images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image, index) => (
-          <div key={image.id + "-" + index} className="gap w-48">
+        {images.map((image, index) => (
+          <div key={image.id + "-" + index} className="gap flex w-48 flex-col">
             <img src={image.url} className="" alt="astrophotography" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
