ec7cd2da2274a8d9e20fd71160c9277b45bf351f
Evan Hill <evanhill1989@gmail.com>
2024-08-06 17:19:10 -0400

    Image Upload

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 - [x] Auth (clerk)
 - [] Add image upload
 - [] "taint" (server-only)
+- [] Use Next/Image
 - [] Error mgmt (sentry)
 - [] Routing/image page ( parallel route )
 - [] Delete button (w/ server actions)
--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,14 +1,16 @@
 import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
-      <div>
+      <div className="flex">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton endpoint="imageUploader" />
           <UserButton />
         </SignedIn>
       </div>
